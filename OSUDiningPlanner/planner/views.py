39589dccae659c42479023561e31c9a79062c649
bobataylor <cam@rtastudio.com>
2017-10-21 12:51:30 -0700

    Various updates this is not a good commit message sorry

--- OSUDiningPlanner/planner/views.py
+++ OSUDiningPlanner/planner/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse("Welcome to the planner!")
+	#context = {}
+	return render(request, 'planner/index.html')
