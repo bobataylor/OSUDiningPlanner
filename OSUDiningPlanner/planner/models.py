bf5e2a25875acafd4d7f784dc731209294768bbe
nishadprabhu <prabhu.44@osu.edu>
2017-10-21 14:05:31 -0400

    Models for DB

--- OSUDiningPlanner/planner/models.py
+++ OSUDiningPlanner/planner/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    portionSize = models.PositiveIntegerField()
+
+
+class Location(models.Model):
